0358c45369c6b462897b6db06a9bb3fbf008154d
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2024-02-11 22:03:51 +0100

    Improved readability, user can enter prefered gridsize

--- js/game.js
+++ js/game.js
@@ -1,25 +1,26 @@
-// user input must be less or equal to 100
-const gridSize = prompt('Enter desired canvas grid (must be less than 100): ');
-if (gridSize > 100) gridSize = 99
-alert('Grid size is 99, you entered a size over 100!');
+const canvas = document.querySelector('#canvas')
 
-/* get the reference to '#gameContainer */
-const canvas = document.querySelector('#canvas');
+// prmompt for the size of the desired grid
+const gridSize = prompt('Enter grid size: ');
+if (gridSize > 100) gridSize = 99;
 
-/* create the 16x16 grid */
 for (let row = 0; row < gridSize; row++) {
     for (let column = 0; column < gridSize; column++) {
+        // create the pixel and style it accordingly
         const pixel = document.createElement('div');
-        // apply css styles to the recent created div (pixel)
         pixel.style.height = '25px';
         pixel.style.width = '25px';
         pixel.style.border = '1px solid transparent';
         pixel.style.boxSizing = 'border-box';
         canvas.appendChild(pixel);
 
-        // add for the recent created div (pixel) the 'mouseover' event
         pixel.addEventListener('mouseover', () => {
             pixel.style.backgroundColor = 'grey';
         });
     }
 }
+
+
+// 
+canvas.style.height = (gridSize * 25) + 'px';
+canvas.style.width = (gridSize * 25) + 'px';
